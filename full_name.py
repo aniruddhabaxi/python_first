9f2e079368494725a35ae0a76318d8b0bd7a76e1
Aniruddha <aniruddhabaxi@gmail.com>
2024-06-10 05:50:47 -0400

    first commit

---
+++ full_name.py
@@ -0,0 +1,6 @@
+first_name = "Ani"
+middle_name = "vishnu"
+last_name = "Baxi"
+full_name = first_name + " " + last_name
+print(f"My full name is {first_name} {middle_name} {last_name}")
+print("My full name is {} {} {}". format(first_name, middle_name, last_name))
