186de1df1d12babe2acf1c46c0192afededc38b1
Aniruddha <aniruddhabaxi@gmail.com>
2024-06-10 06:41:33 -0400

    first commit

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+p = 10
+print(f"The value of p is {p} and datatype is {type(p)}")
+q = "xyz"
+print(f"The q is displayed as {q} and datatype is {type(q)}")
+r = 1.5
+print(f"The value of r is {r} and datatype is {type(r)}")
+s = 9+10j
+print(f"The value of s is {s} and datatype is {type(s)}")
+t = ["Ani", "Vishnu", "Baxi"]
+print(f"The value of t is {t} and datatype is {type(t)}")
+u = range(5)
+print(u)
+v=range(1,6,2)
+for i in v:
+    print(f"the value of v is {i} & The datatype of v is {type(v)}")
+w=True
+print(f"the value of e is {w} and the datatype is {type(w)}")
+
+
